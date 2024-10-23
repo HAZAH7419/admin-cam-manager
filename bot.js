0891888fe2511f736adb44856d28ab0394b785d3
Conner Callaway <conner.callaway@cox.com>
2024-10-22 21:06:21 -0500

    Updated discord bot command

--- bot.js
+++ bot.js
@@ -1,42 +1,47 @@
-import { Client, Events, GatewayIntentBits } from 'discord.js'
+import { Client, Events, GatewayIntentBits } from "discord.js";
 
 export class DiscordBot {
-    constructor(config, state) {
-        this.config = config
+  constructor(config, state) {
+    this.config = config;
 
-        // AppState
-        this.state = state
+    // AppState
+    this.state = state;
 
-        this.client = new Client({ intents: [GatewayIntentBits.Guilds] })
-    
-        this.client.once(Events.ClientReady, (ready) => {
-            console.info(`Bot is ready - ${ready.user.tag}`)
-        })
-    
-        this.client.on(Events.InteractionCreate, (action) => {
-            if (action.isChatInputCommand()) {
-                if (action.commandName === 'set-cam-value') {
-                    const name = action.options.getString('name', true)
-                    const value = action.options.getString('value', true)
-    
-                    // validate...
-    
-                    console.info(`updating flag: ${name}=${value}`)
-                    this.state.flags.set(name, value)
+    this.client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-                    action.reply({ content: "value updated", ephemeral: true })
-                } else if (action.commandName === 'add-streamer') {
-                    const id = action.options.getString('id', true)
+    this.client.once(Events.ClientReady, (ready) => {
+      console.info(`Bot is ready - ${ready.user.tag}`);
+    });
 
-                    this.state.addStreamer(id)
+    this.client.on(Events.InteractionCreate, (action) => {
+      if (action.isChatInputCommand()) {
+        if (action.commandName === "admin-cam-script") {
+          const value = action.options.getString("enable", true);
+          let message;
 
-                    action.reply({ content: 'streamer added', ephemeral: true })
-                }
-            }
-        })
-    }
+          // validate...
 
-    async start() {
-        await this.client.login(this.config.token)
-    }
+          console.info(`updating flag: ${value}`);
+          this.state.flags.set("enable", value);
+
+          message =
+            value === "false"
+              ? "Admin cam script is disabled"
+              : "Admin cam script is enabled";
+
+          action.reply({ content: message, ephemeral: true });
+        } else if (action.commandName === "add-streamer") {
+          const id = action.options.getString("id", true);
+
+          this.state.addStreamer(id);
+
+          action.reply({ content: "streamer added", ephemeral: true });
+        }
+      }
+    });
+  }
+
+  async start() {
+    await this.client.login(this.config.token);
+  }
 }
--- bot.register.js
+++ bot.register.js
@@ -1,20 +1,28 @@
-import { REST, Routes, SlashCommandBuilder } from 'discord.js'
+import { REST, Routes, SlashCommandBuilder } from "discord.js";
 import dotenv from "dotenv";
 dotenv.config();
 
-const token = process.env.DISCORD_TOKEN
-const appId = process.env.DISCORD_APP_ID
+const token = process.env.DISCORD_TOKEN;
+const appId = process.env.DISCORD_APP_ID;
 
-const client = new REST().setToken(token)
+const client = new REST().setToken(token);
 
 const commands = [
-    new SlashCommandBuilder()
-        .setName('set-cam-value')
-        .setDescription('Set a value in the config')
-        .addStringOption(opt => opt.setName('name').setDescription('Name of the value').setRequired(true))
-        .addStringOption(opt => opt.setName('value').setDescription('The value').setRequired(true))
-]
+  new SlashCommandBuilder()
+    .setName("admin-cam-script")
+    .setDescription("Set a value in the config")
+    .addStringOption((opt) =>
+      opt
+        .setName("enable")
+        .setDescription("Name of the value")
+        .setRequired(true)
+        .addChoices(
+          { name: "True", value: "true" },
+          { name: "False", value: "false" }
+        )
+    ),
+];
 
 await client.put(Routes.applicationCommands(appId), {
-    body: commands.map(c => c.toJSON())
-})
+  body: commands.map((c) => c.toJSON()),
+});
