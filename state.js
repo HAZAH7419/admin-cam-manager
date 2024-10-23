b209a0a4cb2254738316d018255f5a9a74f1a0ba
Conner Callaway <conner.callaway@cox.com>
2024-10-23 11:38:14 -0500

    fixed naming issues

--- state.js
+++ state.js
@@ -1,23 +1,23 @@
 export class AppState {
-    constructor() {
-        this.flags = new Map()
-        this.flags.set('enabled', 'true')
-        this.streamerSteamIds = new Set(process.env.STREAMERS?.split(','))
-    }
+  constructor() {
+    this.flags = new Map();
+    this.flags.set("enable", "true");
+    this.streamerSteamIds = new Set(process.env.STREAMERS?.split(","));
+  }
 
-    isEnabled() {
-        return this.flags.get('enabled') !== 'false'
-    }
+  isEnabled() {
+    return this.flags.get("enable") !== "false";
+  }
 
-    isStreamer(id) {
-        return this.streamerSteamIds.has(id)
-    }
+  isStreamer(id) {
+    return this.streamerSteamIds.has(id);
+  }
 
-    addStreamer(id) {
-        this.streamerSteamIds.add(id)
-    }
+  addStreamer(id) {
+    this.streamerSteamIds.add(id);
+  }
 
-    removeStreamer(id) {
-        this.streamerSteamIds.delete(id)
-    }
+  removeStreamer(id) {
+    this.streamerSteamIds.delete(id);
+  }
 }
